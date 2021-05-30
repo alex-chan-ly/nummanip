170e0001be1bf2e4dbd404350355b2e37e3441ec
alex.chan.ly@gmail.com <alex.chan.ly@gmail.com>
2021-05-30 09:00:36 +0000

    Add accept variable arguments

--- calc/math.go
+++ calc/math.go
@@ -1,5 +1,14 @@
 package calc
 
-func Add(i int, j int) int {
-	return i + j
+func Add(numbers ...int) int {
+	sum := 0
+
+	for _, num := range numbers {
+		sum = sum + num
+	}
+	return sum
+}
+
+func Minus(i int, j int) int {
+	return i - j
 }
